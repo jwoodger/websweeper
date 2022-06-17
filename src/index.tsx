dd913cb9a6fe011fa793e8c134af680f22eb5fa6
jwoodger <jwoodger@github.com>
2022-06-17 15:03:52 -0400

    Initial commit.

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+// Copyright (C) 2022, Jack Woodger
+// SPDX-License-Identifier: Apache-2.0
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+    <React.StrictMode>
+        <p>What up?</p>
+    </React.StrictMode>
+);
